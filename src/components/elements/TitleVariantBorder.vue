69a4de68abbcb1843fe1d330fb9714f4a7cba87d
mbank14 <jembar.ashofa01@gmail.com>
2021-09-08 21:59:00 +0700

    add files

---
+++ src/components/elements/TitleVariantBorder.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <slot>
+          <p>
+              {{titleText}}
+          </p>
+          <hr>
+      </slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props: { 
+        titleText: {
+            type: String,
+            default: 'Untitled'
+        }
+    },
+}
+</script>
+
+<style>
+    
+</style>
